39a14590cc291194cefe16ec87cd668b23f9d97e
machin14 <machin14@gmail.com>
2024-07-31 18:40:05 +0300

    values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,18 @@
-
 let js = 'amazing';
-if (js==='amazing') alert('Javascript if FUN!');
-
 console.log(40+8+23-10);
+
+let firstName = "Matilda";
+let first = "jonas";
+let firstNamePerson ="Guy";
+let first_name_person = "Moshe";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+
