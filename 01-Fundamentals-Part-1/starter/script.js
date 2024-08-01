199438c30d3b6117e6aef7a3015aadbecc09e1c8
machin14 <machin14@gmail.com>
2024-08-01 23:58:06 +0300

    learning data types (#6)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
 let js = 'amazing';
-console.log(40+8+23-10);
+// console.log(40+8+23-10);
 
 let firstName = "Matilda";
 let first = "jonas";
@@ -12,7 +12,31 @@ console.log(firstName);
 
 let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
-
 console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+
+
 
 
