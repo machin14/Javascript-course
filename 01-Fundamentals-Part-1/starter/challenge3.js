37b23d7ef2f9b2a9a92f641120a8c6f92cf84a3c
machin14 <machin14@gmail.com>
2024-08-06 18:26:00 +0300

    operators and ternary and switch

---
+++ 01-Fundamentals-Part-1/starter/challenge3.js
@@ -0,0 +1,42 @@
+/*
+const dolphinsOne = 96;
+const dolphinsTwo = 108;
+const dolphinsThree = 89;
+
+const koalasOne = 88;
+const koalasTwo = 91;
+const koalasThree = 110;
+
+const dolphinsAVG = (dolphinsOne + dolphinsTwo + dolphinsThree)/3 ;
+const koalasAVG = (koalasOne + koalasTwo + koalasThree)/3 ;
+console.log(dolphinsAVG,koalasAVG);
+//task 1 and 2
+if(dolphinsAVG > koalasAVG){
+    console.log('Congratulations Dolphins! You are the winners!');
+}else if(koalasAVG > dolphinsAVG){
+    console.log('Congratulations Dolphins! You are the winners!');
+}else{
+    console.log("Good try! But it's a draw!");
+}
+    
+//task 3
+
+const dolphinsOne = 97;
+const dolphinsTwo = 112;
+const dolphinsThree = 101;
+
+const koalasOne = 109;
+const koalasTwo = 95;
+const koalasThree = 106;
+
+const dolphinsAVG = (dolphinsOne + dolphinsTwo + dolphinsThree)/3 ;
+const koalasAVG = (koalasOne + koalasTwo + koalasThree)/3 ;
+
+if(dolphinsAVG > koalasAVG && dolphinsAVG >= 100){
+    console.log('Congratulations Dolphins! You are the winners!');
+}else if(koalasAVG > dolphinsAVG && koalasAVG >= 100){
+    console.log('Congratulations Koalas!! You are the winners!');
+}else if(dolphinsAVG === koalasAVG && koalasAVG >= 100 && dolphinsAVG >= 100){
+    console.log("Good try! But it's a draw!");
+}
+    */
